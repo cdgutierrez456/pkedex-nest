3bcd9113b408c75675a351292c496744fd0e14d0
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-02-10 21:06:40 -0500

    Ejecutando logica en servicio para creacion de pokemon, incluyendo validaciones

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,13 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Pokemon } from './entities/pokemon.entity';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+
 
 @Injectable()
 export class PokemonService {
 
-  create(createPokemonDto: CreatePokemonDto) {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase()
-    return createPokemonDto;
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto)
+      return pokemon;
+    } catch (error) {
+      if (error.code === 1100) {
+        throw new BadRequestException('Pokemon exist in DB')
+      }
+      throw new InternalServerErrorException("Can't create Pokemon")
+    }
+
   }
 
   findAll() {
