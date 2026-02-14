88471b9b1c8fdeec93ea84a0c65fc582f1da49f9
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-02-14 18:13:47 -0500

    Implementando patron adaptador para axios:

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
+
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { PokemonService } from 'src/pokemon/pokemon.service';
@@ -7,13 +8,16 @@ import { PokemonService } from 'src/pokemon/pokemon.service';
 @Injectable()
 export class SeedService {
 
-  constructor(private readonly pokemonService: PokemonService) {}
+  constructor(
+    private readonly pokemonService: PokemonService,
+    private readonly http: AxiosAdapter
+  ) {}
 
-  private readonly axios: AxiosInstance = axios
+
 
   async executeSeed() {
     await this.pokemonService.removeAll()
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=700')
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=700')
 
     const pokemonToInsert: { no: number, name: string }[] = [];
 
