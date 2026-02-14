4c05aea8007458de0401e67aba9c3855556bbdd1
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-02-14 16:32:57 -0500

    Consumo de servicio http externo para captura de informacion de posterior insercion en db

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
 
-  executeSeed() {
-    return 'Seed executed'
+  private readonly axios: AxiosInstance = axios
+
+  async executeSeed() {
+    // https://pokeapi.co/api/v2/pokemon?limit=700
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=700')
+
+    data.results.forEach(({ name, url }) => {
+      console.log({ name, url });
+
+    })
+
+    return data.results;
   }
 
 }
